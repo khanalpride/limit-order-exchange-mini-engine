3dac4f4e300abf36f22ea917a307fc872bb021a3
Santosh Khanal <santosh.khanal55@gmail.com>
2025-12-12 20:57:47 +0545

    add api token login endpoint

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\Api\OrderController;
 use App\Http\Controllers\Api\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+Route::post('login', [App\Http\Controllers\Api\LoginController::class, 'login'])->name('api.login');
+
 Route::get('profile', ProfileController::class)
     ->middleware('auth:sanctum')
     ->name('api.profile');
