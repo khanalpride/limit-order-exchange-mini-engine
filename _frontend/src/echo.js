7e5363d1aac9b6ac0a6941c74a989470aece7c09
Santosh Khanal <santosh.khanal55@gmail.com>
2025-12-12 22:42:09 +0545

    add readme and switch to pusher api

--- _frontend/src/echo.js
+++ _frontend/src/echo.js
@@ -4,15 +4,11 @@ import Pusher from 'pusher-js'
 window.Pusher = Pusher
 
 const echo = new Echo({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-    enabledTransports: ['ws', 'wss'],
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
+    forceTLS: true,
     authEndpoint: `${import.meta.env.VITE_API_URL.replace('/api', '')}/broadcasting/auth`,
-    withCredentials: false,
     auth: {
         headers: {
             Authorization: `Bearer ${localStorage.getItem('auth_token')}`,
